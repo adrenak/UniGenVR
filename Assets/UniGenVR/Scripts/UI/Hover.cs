144c3bbc3a8184487a9e3d903d8873f832eabee0
adrenak <vatsalatfei@gmail.com>
2018-02-27 14:29:39 +0530

    - Rename BoundTransform to Hover - Improve Hover.cs with customization height and distance

--- Assets/UniGenVR/Scripts/UI/Hover.cs
+++ Assets/UniGenVR/Scripts/UI/Hover.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 using UniPrep.Utils;
 
 namespace UniGenVR.UI {
-    // This class is used to move UI elements in ways that are
-    // generally useful when using VR, specifically looking at
-    // the camera and rotating so they're always in front of
-    // the camera.
-    public class BoundTransform : MonoBehaviour {
-        [SerializeField] Transform m_CameraTransform;
+    public class Hover : MonoBehaviour {
+        [SerializeField] Transform m_SubjectTransform;
         [SerializeField] float m_FollowSpeed = 2;
 
-        float m_DistanceFromCamera;
+        [SerializeField] float m_DistanceFromSubject;
+        [SerializeField] float m_HeightFromSubject;
+
         float m_OriginalFollowSpeed;
 
         /// <summary>
@@ -40,28 +38,25 @@ namespace UniGenVR.UI {
             m_FollowSpeed = m_OriginalFollowSpeed;
         }
 
-        void Start() {
-            m_DistanceFromCamera = Vector3.Distance(transform.position, m_CameraTransform.position);
+        void Awake() {
             m_OriginalFollowSpeed = m_FollowSpeed;
         }
 
         void Update() {
             // Set it's rotation to point from the UI to the camera.
-            transform.rotation = Quaternion.LookRotation(transform.position - m_CameraTransform.position);
+            transform.rotation = Quaternion.LookRotation(transform.position - m_SubjectTransform.position);
 
             // Find the direction the camera is looking but on a flat plane.
-            Vector3 targetDirection = Vector3.ProjectOnPlane(m_CameraTransform.forward, Vector3.up).normalized;
+            Vector3 targetDirection = Vector3.ProjectOnPlane(m_SubjectTransform.forward, Vector3.up).normalized;
 
             // Calculate a target position from the camera in the direction at the same distance from the camera as it was at Start.
-            Vector3 targetPosition = m_CameraTransform.position + targetDirection * m_DistanceFromCamera;
+            Vector3 targetPosition = m_SubjectTransform.position + targetDirection * m_DistanceFromSubject;
+            targetPosition.y = m_SubjectTransform.position.y + m_HeightFromSubject;
 
             // Set the target position  to be an interpolation of itself and the UI's position.
             targetPosition = Vector3.Lerp(transform.position, targetPosition, m_FollowSpeed * Time.deltaTime);
 
-            // Since the UI is only following on the XZ plane, negate any y movement.
-            targetPosition.y = transform.position.y;
-
-            // Set the UI's position to the calculated target position.
+            // Set the position to the calculated target position.
             transform.position = targetPosition;
         }
     }
