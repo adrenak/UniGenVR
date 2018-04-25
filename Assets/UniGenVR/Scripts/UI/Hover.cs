0bd2e36b47ff7bc8e28034949fb2a742ab098b69
adrenak <vatsalatfei@gmail.com>
2018-04-26 03:30:17 +0530

    Correct Hover.cs

--- Assets/UniGenVR/Scripts/UI/Hover.cs
+++ Assets/UniGenVR/Scripts/UI/Hover.cs
@@ -7,8 +7,13 @@ namespace UniGenVR.UI {
         [SerializeField] Transform m_SubjectTransform;
 
         [SerializeField] float m_Distance;
-        [Range(-90, 90)] [SerializeField] float m_Pitch;
-        [Range(-180, 190)] [SerializeField] float m_Yaw;
+
+        [Range(-90, 90)]
+        [SerializeField] float m_Pitch;
+
+        [Range(-180, 190)]
+        [SerializeField] float m_Yaw;
+
         [SerializeField] float m_FollowRate = 2;
 
         float m_OriginalFollowSpeed;
@@ -47,15 +52,17 @@ namespace UniGenVR.UI {
         void Update() {
             // Clamp pitch and yaw
             m_Pitch = Mathf.Clamp(m_Pitch, -90, 90);
+
             m_Yaw = Mathf.Clamp(m_Yaw, -180, 180);
+            var adjustedYaw = m_SubjectTransform.localEulerAngles.y + m_Yaw;
 
             // Set it's rotation to point from the UI to the camera.
             transform.rotation = Quaternion.LookRotation(transform.position - m_SubjectTransform.position);
 
             Vector3 targetPosition = new Vector3(
-                m_SubjectTransform.position.x + (m_Distance * Mathf.Cos(Mathf.Deg2Rad * m_Pitch) * Mathf.Sin(Mathf.Deg2Rad * m_Yaw)),
+                m_SubjectTransform.position.x + (m_Distance * Mathf.Cos(Mathf.Deg2Rad * m_Pitch) * Mathf.Sin(Mathf.Deg2Rad * adjustedYaw)),
                 m_SubjectTransform.position.y + (m_Distance * Mathf.Sin(Mathf.Deg2Rad * m_Pitch)),
-                m_SubjectTransform.position.z + (m_Distance * Mathf.Cos(Mathf.Deg2Rad * m_Pitch) * Mathf.Cos(Mathf.Deg2Rad * m_Yaw))
+                m_SubjectTransform.position.z + (m_Distance * Mathf.Cos(Mathf.Deg2Rad * m_Pitch) * Mathf.Cos(Mathf.Deg2Rad * adjustedYaw))
             );
 
             // Set the target position to be an interpolation of itself and the UI's position.
